fe19c7b97fb9b104e0d2736e3a3196151bda42d7
Edison Mora <edison.mora2802@hotmail.com>
2015-11-07 21:57:33 -0600

    refactor some files

--- server.js
+++ server.js
@@ -2,16 +2,7 @@ var express = require('express');
 var app = express();
 var PORT = 3000;
 
-var middleware = {
-	requireAuth: function(req, res, next){
-		console.log('private route hit');
-		next();
-	},
-	logger: function(req, res, next){
-		console.log('Request: ' + req.method + ' ' + req.originalUrl + ' on ' + new Date().toString());
-		next();
-	}
-}
+var middleware = require('./middleware');
 
 app.use(middleware.logger);
 app.use(express.static(__dirname + '/public'));

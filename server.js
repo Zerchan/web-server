ba9004bd94b18c00717a3859650bf9112c191fae
Edison Mora <edison.mora2802@hotmail.com>
2015-11-07 21:48:32 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+var middleware = {
+	requireAuth: function(req, res, next){
+		console.log('private route hit');
+		next();
+	},
+	logger: function(req, res, next){
+		console.log('Request: ' + req.method + ' ' + req.originalUrl + ' on ' + new Date().toString());
+		next();
+	}
+}
+
+app.use(middleware.logger);
+app.use(express.static(__dirname + '/public'));
+
+app.get('/about', middleware.requireAuth, function(req, res){
+	res.send('About Us');
+});
+
+app.listen(PORT, function(){
+	console.log('Server listen to port ' + PORT);
+});

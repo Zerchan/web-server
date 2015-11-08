dae82e4bcbc6f2eb0a056c8fd775924aed905c13
Edison Mora <edison.mora2802@hotmail.com>
2015-11-07 21:57:50 -0600

    refactor some files

---
+++ middleware.js
@@ -0,0 +1,10 @@
+module.exports = {
+	requireAuth: function(req, res, next){
+		console.log('private route hit');
+		next();
+	},
+	logger: function(req, res, next){
+		console.log('Request: ' + req.method + ' ' + req.originalUrl + ' on ' + new Date().toString());
+		next();
+	}
+};
